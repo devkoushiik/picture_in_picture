b5eb87243f4b42c74582e23271f08caca11500f2
Koushik Ahmed <koushikxahmed@gmail.com>
2023-03-05 15:15:56 +0600

    picture in picture project

---
+++ app.js
@@ -0,0 +1,29 @@
+//
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch error here
+    console.log("Whoops, error here: ", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  // Disable button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // reset button
+  button.disabled = false;
+});
+
+// onload;
+selectMediaStream();
